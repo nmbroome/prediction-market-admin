3be2aaf0be0701594372aa8bf9f6a75cfae6bff6
dajpkov <79390298+dajpkov@users.noreply.github.com>
2025-01-03 23:06:20 -0500

    Add profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+export default function UserProfile() {
+    <div className="container mt-4">
+        <h2>Your Profile</h2>
+        <div className="mt-4">
+            <div className="flex-col">
+                <p>Email: </p>
+                <p>Balance: </p>
+                <p>Player ID: </p>
+            </div>
+            <div className="flex-col">
+                <p>Payment info</p>
+            </div>
+            <h3>Holdings</h3>
+            <h3>Prediction history</h3>
+        </div>
+    </div>
+}
